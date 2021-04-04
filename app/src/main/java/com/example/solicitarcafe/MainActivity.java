84e18a307fa6e6e362ec19625cac1247a103086b
Bruno dos Santos Alves <devbrunosantos@gmail.com>
2021-04-04 17:11:34 -0300

    Aplicativo simples de aprendizagem e focado em layout/funções.

--- app/src/main/java/com/example/solicitarcafe/MainActivity.java
+++ app/src/main/java/com/example/solicitarcafe/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.solicitarcafe;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,6 @@ import java.text.NumberFormat;
  * Este aplicativo exibe um pedido de café.
  */
 public class MainActivity extends AppCompatActivity {
-
     int quantidade = 0;
 
     @Override
@@ -25,40 +26,66 @@ public class MainActivity extends AppCompatActivity {
      * Este método é ativado quando o botão "+" é clicado.
      */
     public void adicao(View view) {
+        if (quantidade == 100) {
+            return;
+        }
         quantidade = quantidade + 1;
-        display(quantidade);
+        mostrarQuantidade(quantidade);
     }
 
     /**
      * Este método é ativado quando o botão "-" é clicado.
      */
     public void subtracao(View view) {
+        if (quantidade == 1) {
+            return;
+        }
         quantidade = quantidade - 1;
-        display(quantidade);
+        mostrarQuantidade(quantidade);
     }
 
     /**
      * Este método é ativado quando o botão de "atualizar o carrinho" é clicado.
      */
     public void atualizar(View view) {
-        displayPrice(quantidade * 5);
+        EditText nomeUsuario = (EditText) findViewById(R.id.nome_usuario);
+        String nome = nomeUsuario.getText().toString();
+        CheckBox copoDescartavel = (CheckBox) findViewById(R.id.copo_descartavel);
+        boolean adicionarDescartavel = copoDescartavel.isChecked();
+        CheckBox comLeite = (CheckBox) findViewById(R.id.com_leite);
+        boolean adicionarLeite = comLeite.isChecked();
+        int preco = calcularPreco();
+        String mensagemValor = resumoPedido(nome, preco, adicionarDescartavel, adicionarLeite);
+        mostrarMensagem(mensagemValor);
+    }
+
+    private int calcularPreco() {
+        return quantidade * 5;
+    }
+
+    private String resumoPedido(String nome, int preco, boolean adicionarDescartavel, boolean adicionarLeite) {
+        String mensagemValor = "Nome: " + nome;
+        mensagemValor += "\nQuantidade: " + quantidade;
+        mensagemValor += "\nAdicionar copo descartável? " + adicionarDescartavel;
+        mensagemValor += "\nAdicionar leite? " + adicionarLeite;
+        mensagemValor += "\nTotal: R$" + calcularPreco();
+        mensagemValor += "\n\nObrigado e volte sempre!";
+        return mensagemValor;
     }
 
     /**
      * Este método exibe o valor de quantidade fornecido na tela.
      */
-    private void display(int number) {
+    private void mostrarQuantidade(int numeroCafes) {
         TextView quantidadeTextView = (TextView) findViewById(
                 R.id.quantidade);
-        quantidadeTextView.setText("" + number);
+        quantidadeTextView.setText("" + numeroCafes);
     }
 
-    /**
-     * Este método exibe o valor do preço fornecido na tela.
-     */
-    private void displayPrice(int number) {
-        TextView valorTotal = (TextView) findViewById(R.id.valorTotal);
-        valorTotal.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void mostrarMensagem(String message) {
+        TextView resumoDoPedido = (TextView) findViewById(
+                R.id.resumoDoPedido);
+        resumoDoPedido.setText(message);
     }
 
 }
